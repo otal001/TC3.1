c4e7362fa3ca3af1692aa42c27b722955abd8c6f
otalo001@umn.edu <otalo001@umn.edu>
2023-11-03 16:35:15 -0500

    initial project commit

--- out/webhelp-responsive/oxygen-webhelp/app/search/index/link-to-parent.js
+++ out/webhelp-responsive/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -1,2 +1,2 @@
 /*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
-var linkToParent = {11:15,13:-1,9:16,4:15,8:15,1:-1,5:15,14:15,3:1,6:13,7:13,12:16,15:-1,2:1,10:1,16:-1};
+var linkToParent = {4:15,16:-1,9:16,8:15,6:13,5:15,3:1,1:-1,14:15,10:1,12:16,15:-1,11:15,13:-1,2:1,7:13};
