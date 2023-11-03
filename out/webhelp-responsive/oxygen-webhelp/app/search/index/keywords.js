05e1002c04f4cb9472b999884be78b8e8bc2a804
otalo001@umn.edu <otalo001@umn.edu>
2023-11-03 15:37:11 -0500

    commit

---
+++ out/webhelp-responsive/oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,23 @@
+var keywords=[{w:"Context",p:["p0"]},{w:"Sensitive",p:["p0"]},{w:"Help",p:["p0"]},{w:"Create",p:["p1","p2","p3","p15"]},{w:"and",p:["p1","p5","p6","p10","p13","p16"]},{w:"edit",p:["p1"]},{w:"content",p:["p1","p4","p14"]},{w:"a",p:["p2","p3","p15"]},{w:"News",p:["p2"]},{w:"Article",p:["p2","p4","p8","p14","p15"]},{w:"Service",p:["p3"]},{w:"Page",p:["p3"]},{w:"Enter",p:["p4","p14"]},{w:"into",p:["p4"]},{w:"the",p:["p4","p9","p12","p16"]},{w:"Body",p:["p4","p14"]},{w:"field",p:["p4","p14"]},{w:"Format",p:["p5"]},{w:"Internal",p:["p5"]},{w:"Public-Facing",p:["p5"]},{w:"Articles",p:["p5","p11"]},{w:"How",p:["p6"]},{w:"Linking",p:["p6","p13"]},{w:"Works",p:["p6"]},{w:"Between",p:["p6"]},{w:"TDX",p:["p6","p7","p13"]},{w:"Drupal",p:["p6"]},{w:"knowledge",p:["p7","p9","p12","p13","p15","p16"]},{w:"base",p:["p7","p9","p12","p13","p15","p16"]},{w:"display",p:["p7","p13"]},{w:"List",p:["p8"]},{w:"of",p:["p8"]},{w:"types",p:["p8"]},{w:"Navigate",p:["p9"]},{w:"to",p:["p9"]},{w:"Roles",p:["p10"]},{w:"editing",p:["p10"]},{w:"permissions",p:["p10"]},{w:"Search",p:["p11","p12","p16"]},{w:"for",p:["p11"]},{w:"duplicate",p:["p11"]},{w:"New",p:["p15"]},{w:"Open",p:["p16"]}];
+var ph={};
+ph["p0"]=[0, 1, 2];
+ph["p1"]=[3, 4, 5, 6];
+ph["p2"]=[3, 7, 8, 9];
+ph["p3"]=[3, 7, 10, 11];
+ph["p4"]=[12, 9, 6, 13, 14, 15, 16];
+ph["p5"]=[17, 18, 4, 19, 20];
+ph["p6"]=[21, 22, 23, 24, 25, 4, 26];
+ph["p7"]=[25, 27, 28, 29];
+ph["p8"]=[30, 31, 9, 32];
+ph["p9"]=[33, 34, 14, 27, 28];
+ph["p10"]=[35, 4, 36, 37];
+ph["p12"]=[38, 14, 27, 28];
+ph["p11"]=[38, 39, 40, 20];
+ph["p14"]=[12, 15, 16, 9, 6];
+ph["p13"]=[25, 27, 28, 29, 4, 22];
+ph["p16"]=[42, 4, 38, 14, 27, 28];
+ph["p15"]=[3, 7, 41, 27, 28, 9];
+var keywordsInfo = {
+    keywords: keywords,
+    ph: ph
+}
---
+++ out/webhelp-responsive/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -0,0 +1,2 @@
+/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
+var linkToParent = {11:15,13:-1,9:16,4:15,8:15,1:-1,5:15,14:15,3:1,6:13,7:13,12:16,15:-1,2:1,10:1,16:-1};
