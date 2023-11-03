4150f6a08118b91a9f831bb6716a271ba7d6e251
otalo001@umn.edu <otalo001@umn.edu>
2023-11-03 17:26:26 -0500

    initial project commit

--- docs/oxygen-webhelp/app/search/index/keywords.js
+++ docs/oxygen-webhelp/app/search/index/keywords.js
@@ -1,4 +1,4 @@
-var keywords=[{w:"Context",p:["p0"]},{w:"Sensitive",p:["p0"]},{w:"Help",p:["p0"]},{w:"Creating",p:["p1","p2"]},{w:"a",p:["p1","p2"]},{w:"News",p:["p1"]},{w:"Article",p:["p1"]},{w:"Service",p:["p2"]},{w:"Page",p:["p2"]},{w:"How",p:["p3"]},{w:"Linking",p:["p3"]},{w:"Works",p:["p3"]},{w:"Between",p:["p3"]},{w:"TDX",p:["p3","p4"]},{w:"and",p:["p3","p5"]},{w:"Drupal",p:["p3"]},{w:"knowledge",p:["p4"]},{w:"base",p:["p4"]},{w:"display",p:["p4"]},{w:"Roles",p:["p5"]},{w:"Editing",p:["p5"]},{w:"Permissions",p:["p5"]}];
+var keywords=[{w:"Context",p:["p0"]},{w:"Sensitive",p:["p0"]},{w:"Help",p:["p0"]},{w:"Creating",p:["p1","p2"]},{w:"a",p:["p1","p2","p7","p8","p17"]},{w:"News",p:["p1","p7"]},{w:"Article",p:["p1","p7","p9","p11","p16","p17"]},{w:"Service",p:["p2","p8"]},{w:"Page",p:["p2","p8"]},{w:"How",p:["p3"]},{w:"Linking",p:["p3","p15"]},{w:"Works",p:["p3"]},{w:"Between",p:["p3"]},{w:"TDX",p:["p3","p4","p15"]},{w:"and",p:["p3","p5","p6","p10","p15","p18"]},{w:"Drupal",p:["p3"]},{w:"knowledge",p:["p4","p12","p14","p15","p17","p18"]},{w:"base",p:["p4","p12","p14","p15","p17","p18"]},{w:"display",p:["p4","p15"]},{w:"Roles",p:["p5"]},{w:"Editing",p:["p5"]},{w:"Permissions",p:["p5"]},{w:"Create",p:["p6","p7","p8","p17"]},{w:"edit",p:["p6"]},{w:"content",p:["p6","p9","p16"]},{w:"Enter",p:["p9","p16"]},{w:"into",p:["p9"]},{w:"the",p:["p9","p12","p14","p18"]},{w:"Body",p:["p9","p16"]},{w:"field",p:["p9","p16"]},{w:"Format",p:["p10"]},{w:"Internal",p:["p10"]},{w:"Public-Facing",p:["p10"]},{w:"Articles",p:["p10","p13"]},{w:"List",p:["p11"]},{w:"of",p:["p11"]},{w:"types",p:["p11"]},{w:"Navigate",p:["p12"]},{w:"to",p:["p12"]},{w:"Search",p:["p13","p14","p18"]},{w:"for",p:["p13"]},{w:"duplicate",p:["p13"]},{w:"New",p:["p17"]},{w:"Open",p:["p18"]}];
 var ph={};
 ph["p0"]=[0, 1, 2];
 ph["p1"]=[3, 4, 5, 6];
@@ -6,6 +6,19 @@ ph["p2"]=[3, 4, 7, 8];
 ph["p3"]=[9, 10, 11, 12, 13, 14, 15];
 ph["p4"]=[13, 16, 17, 18];
 ph["p5"]=[19, 14, 20, 21];
+ph["p6"]=[22, 14, 23, 24];
+ph["p7"]=[22, 4, 5, 6];
+ph["p8"]=[22, 4, 7, 8];
+ph["p9"]=[25, 6, 24, 26, 27, 28, 29];
+ph["p10"]=[30, 31, 14, 32, 33];
+ph["p12"]=[37, 38, 27, 16, 17];
+ph["p11"]=[34, 35, 6, 36];
+ph["p14"]=[39, 27, 16, 17];
+ph["p13"]=[39, 40, 41, 33];
+ph["p16"]=[25, 28, 29, 6, 24];
+ph["p15"]=[13, 16, 17, 18, 14, 10];
+ph["p18"]=[43, 14, 39, 27, 16, 17];
+ph["p17"]=[22, 4, 42, 16, 17, 6];
 var keywordsInfo = {
     keywords: keywords,
     ph: ph
--- docs/oxygen-webhelp/app/search/index/link-to-parent.js
+++ docs/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -1,2 +1,2 @@
 /*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
-var linkToParent = {4:3,3:-1,1:-1,5:-1,2:1};
+var linkToParent = {9:20,21:-1,14:21,13:20,11:18,10:20,8:6,6:-1,19:20,15:6,17:21,20:-1,16:20,18:-1,7:6,12:18};
